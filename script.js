6a0ade607b4915ad482a708e48666e998cb32f1f
druin011 <162886093+druin011@users.noreply.github.com>
2024-04-07 16:17:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.onload = function (){
+    let result = {} ;
+    let step = 0 ;
 
+    function showQuestion (questionNumber){
+        document.querySelector(".question").innerHTML = quiz[step]['q'];
+        let answer = '';
+        for (let key in quiz[step]['a']){
+            answer += `<li data-v="${key}">${quiz[step]['a'][key]}</li>` ;
+        }
+        document.querySelector('.answer_option').innerHTML = answer;
+    }
+
+    showQuestion(step);
+}
