b5b96e024866af0b20b9a670dc85bb0b7e0ef100
druin011 <162886093+druin011@users.noreply.github.com>
2024-05-05 16:10:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,54 @@ window.onload = function (){
         document.querySelector(".question").innerHTML = quiz[step]['q'];
         let answer = '';
         for (let key in quiz[step]['a']){
-            answer += `<li data-v="${key}">${quiz[step]['a'][key]}</li>` ;
+            answer += `<li data-v="${key}"class="answer-variant">${quiz[step]['a'][key]}</li>` ;
         }
-        document.querySelector('.answer_option').innerHTML = answer;
+        document.querySelector('.answer').innerHTML = answer;
+    }
+
+    document.onclick = function(event) { 
+        event.stopPropagation();
+        if (event.target.classList.contains('answer-variant') && step < quiz.length){
+            // console.log(even.target);
+            if (result[event.target.dataset.v] != undefined) {
+                result[event.target.dataset.v]++;
+            }
+            else {
+                result[event.target.dataset.v] = 0
+            }
+            step++;
+            if (step == quiz.length){
+                document.querySelector('.question').remove();
+                document.querySelector('.answer').remove();
+                showResult();
+            }
+            else {
+                showQuestion(step)
+            }
+        }
+        console.log(result);
+    }
+
+    function showResult(){
+        let key = Object.keys(result).reduce(function(a,b){
+            return result[a] > result[b] ? a : b;
+        });
+        console.log(key);
+
+        let div = document.createElement('div');
+        div.classList.add('result')
+        div.innerHTML = answers[key]['description'];
+        document.querySelector('main').appendChild(div);
     }
 
     showQuestion(step);
 }
+
+let img = document.querySelector(`img`)
+
+img.addEventListener('mousemove', function(e)) {
+    let dx = e.pageX = window.innerWidth / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    block.style.transform = `rotateX($(angleY)deg)  rotateY($(angleX)deg)`
+}
+
